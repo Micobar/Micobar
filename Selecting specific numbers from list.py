af5fff413b0ca8b984ecfb07fdf67388966423e6
Micobar <149310653+Micobar@users.noreply.github.com>
2023-12-19 18:49:32 +0000

    Add files via upload

---
+++ Selecting specific numbers from list.py
@@ -0,0 +1,10 @@
+data = [10,15,20,29,25,60,34,5,11,27,29,43]
+
+dataset = []
+
+for value in data:
+    if value >= 20:
+        dataset.append(value)
+print(dataset)
+
+# program is deleting digits lower than 20
